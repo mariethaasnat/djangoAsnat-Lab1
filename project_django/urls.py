4aa7d82b0cc6bf089c65b4cc5c35d153fde16f6b
marietha.asnat <marietha.asnat@ui.ac.id>
2022-09-22 14:15:10 +0700

    Upload Lab 3 PBP

--- project_django/urls.py
+++ project_django/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from wishlist.views import login_user, logout_user, register
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example_app.urls')),
-    path('wishlist/', include('wishlist.urls')),
+    path('wishlist/', include('wishlist.urls')),    
 ]
